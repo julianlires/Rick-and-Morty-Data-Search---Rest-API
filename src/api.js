f3f443734ae34c46775385eb95d5ebae968158c3
jlires <jlires@uc.cl>
2021-01-25 20:15:03 -0300

    Changed API endpoint

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 
-const API = 'https://integracion-rick-morty-api.herokuapp.com/api/';
+const API = 'https://rickandmortyapi.com/api/';
 
 export const searchEpisode = (query, page) => {
   let searchEpisodesUrl;
